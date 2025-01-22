d5e9aaf7e7684101a95ec266e6cad907de8f935b
pavel <pavel@localhost.localdomain>
2025-01-22 10:32:25 +0300

    Add pagination, add raw movie_info page

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from .config import Config
 from .routes import register_routes
 
 def create_app(config_info = Config):
-    app = Flask(__name__, template_folder='../../frontend/templates', static_folder='../../frontend/static')
+    app = Flask(__name__, static_folder='../../frontend/static', template_folder='../../frontend/templates')
     app.config.from_object(config_info)
 
     db.init_app(app)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -7,18 +7,25 @@ def register_routes(app: Flask):
 
     @app.route("/")
     def home():
-        movies = Movie.query.all()
-        return render_template('index.html', movies=movies)
+        page = request.args.get("page", 1, type=int)
+        per_page = 20
+
+        sort_by = request.args.get('sort_by', 'popularity') #popularity - сортировка по умолчанию
+
+        valid_sort_fields = ['title', 'release_date', 'popularity']
+        if sort_by not in valid_sort_fields:
+            sort_by = 'popularity' #если поле не валидное, то сортировка по popularity
+
+        movies = Movie.query.order_by(getattr(Movie, sort_by).desc()).paginate(page=page, per_page=per_page)
+        return render_template('main/index.html', movies=movies, sort_by = sort_by)
+
+    @app.route("/<int:movie_id>")
+    def movie_info(movie_id):
+        movie = Movie.query.get(movie_id)  # получение фильма, о котором нудны детали
+        return render_template('info/base.html', movie=movie)
 
     @app.route("/search")
     def search():
         query = request.args.get('q', '')
-        results = Movie.query.filter(Movie.title.ilike(f"%{query}%")).all()
-        return render_template('search.html', results=results)
-
-    @app.route("/addMovie/<id_par>")
-    def add_movie(id_par):
-        temp_movie = Movie(id = int(id_par), title = f"movie{id_par}", release_date = 2024, genre = 'action')
-        db.session.add(temp_movie)
-        db.session.commit()
-        return "Movie added!"
+        movies = Movie.query.filter(Movie.keywords.ilike(f"%{query}%")).all()
+        return render_template('main/search.html', movies=movies)
